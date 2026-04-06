b078540b8d6f42230643ec1e6fe4709c3b995602
bcot-code <78989054+bcot-code@users.noreply.github.com>
2026-04-06 17:11:03 -0500

    init onboarding

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
